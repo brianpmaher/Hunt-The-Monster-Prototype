6656b07571d0e9a806441f40bab08ff4721c2ff0
Brian Maher <bmaher91@gmail.com>
2021-04-05 07:03:18 -0600

    Add first-person look and tweak movement

---
+++ Assets/_Project/Scripts/CursorLockBehavior.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using Cursor = UnityEngine.Cursor;
+
+namespace HuntTheMonster
+{
+    public class CursorLockBehavior : MonoBehaviour
+    {
+        private void Update()
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                LockCursor();
+            }
+
+            if (Input.GetButtonDown("Cancel"))
+            {
+                UnlockCursor();
+            }
+        }
+
+        private void LockCursor()
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        private void UnlockCursor()
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/MouseLookBehavior.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace HuntTheMonster
+{
+    public class MouseLookBehavior : MonoBehaviour
+    {
+        [SerializeField] private Camera firstPersonCamera;
+        [SerializeField] private float verticalLookSpeed = 5f;
+        [SerializeField] private float horizontalLookSpeed = 5f;
+
+        private void Update()
+        {
+            Look();
+        }
+
+        private void Look()
+        {
+            var horizontal = Input.GetAxis("Mouse X") * horizontalLookSpeed * Time.deltaTime;
+            var vertical = -Input.GetAxis("Mouse Y") * verticalLookSpeed * Time.deltaTime;
+            var cameraRotation = new Vector3(vertical, 0, 0);
+            var transformRotation = new Vector3(0, horizontal, 0);
+            
+            firstPersonCamera.transform.Rotate(cameraRotation);
+            transform.Rotate(transformRotation);
+        }
+    }
+}
