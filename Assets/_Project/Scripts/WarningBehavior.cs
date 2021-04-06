dd406fc08758b55a1194c88439c919aad3899136
Brian Maher <bmaher91@gmail.com>
2021-04-06 07:18:13 -0600

    Add adjacent room triggers

---
+++ Assets/_Project/Scripts/WarningBehavior.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace HuntTheMonster
+{
+    public class WarningBehavior : MonoBehaviour
+    {
+        [SerializeField] private string message;
+        
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                Debug.Log(message);
+            }
+        }
+    }
+}
