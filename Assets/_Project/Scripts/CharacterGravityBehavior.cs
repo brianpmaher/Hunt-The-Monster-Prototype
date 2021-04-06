84b82f9e2962983891ffa4f37abc9e1df4eca6cd
Brian Maher <bmaher91@gmail.com>
2021-04-06 07:28:58 -0600

    Add character gravity

---
+++ Assets/_Project/Scripts/CharacterGravityBehavior.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace HuntTheMonster
+{
+    [RequireComponent(typeof(CharacterController))]
+    public class CharacterGravityBehavior : MonoBehaviour
+    {
+        private CharacterController _controller;
+        
+        private bool NotGrounded => !_controller.isGrounded;
+
+        private void Awake()
+        {
+            _controller = GetComponent<CharacterController>();
+        }
+
+        private void FixedUpdate()
+        {
+            if (NotGrounded)
+            {
+                ApplyGravity();
+            }
+        }
+
+        private void ApplyGravity()
+        {
+            var movement = Physics.gravity * Time.fixedDeltaTime;
+            _controller.Move(movement);
+        }
+    }
+}
