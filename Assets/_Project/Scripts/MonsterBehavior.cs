50340bb74131e8ec1b12147253f63b959578c6a5
Brian Maher <bmaher91@gmail.com>
2021-04-09 07:23:29 -0600

    Monster faces the player

---
+++ Assets/_Project/Scripts/MonsterBehavior.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace HuntTheMonster
+{
+    public class MonsterBehavior : MonoBehaviour
+    {
+        private Transform _playerTransform;
+        
+        private void Start()
+        {
+            _playerTransform = PlayerManagerBehavior.PlayerTransform;
+        }
+
+        private void Update()
+        {
+            FaceThePlayer();
+        }
+
+        private void FaceThePlayer()
+        {
+            transform.LookAt(_playerTransform);
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/PlayerManagerBehavior.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace HuntTheMonster
+{
+    public class PlayerManagerBehavior : MonoBehaviour
+    {
+        public static Transform PlayerTransform { get; private set; }
+
+        private void Awake()
+        {
+            PlayerTransform = transform;
+        }
+    }
+}
