91b9b8626b37f752ac44367ae78abcc741bf9b45
Brian Maher <bmaher91@gmail.com>
2021-04-10 14:29:58 -0600

    Add trap trigger

---
+++ Assets/_Project/Scripts/Environment/TrapTrigger.cs
@@ -0,0 +1,18 @@
+﻿using HuntTheMonster.EventChannels;
+using UnityEngine;
+
+namespace HuntTheMonster.Environment
+{
+    public class TrapTrigger : MonoBehaviour
+    {
+        [SerializeField] private VoidEventChannel gameOverEventChannel;
+        
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                gameOverEventChannel.RaiseEvent();
+            }
+        }
+    }
+}
