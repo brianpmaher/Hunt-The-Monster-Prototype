8905731b924f5461d33f649e97c0657e8b45df57
Brian Maher <bmaher91@gmail.com>
2021-04-11 14:35:54 -0600

    Generate level and place floors

---
+++ Assets/_Project/Scripts/LevelGeneration/Level.cs
@@ -0,0 +1,12 @@
+﻿namespace HuntTheMonster.LevelGeneration
+{
+    public class Level
+    {
+        public Room[,] rooms { get; }
+        
+        public Level(int width, int length)
+        {
+            rooms = new Room[width, length];
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/LevelGeneration/LevelGenerator.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace HuntTheMonster.LevelGeneration
+{
+    public class LevelGenerator : MonoBehaviour
+    {
+        [SerializeField] private int levelWidth = 5;
+        [SerializeField] private int levelLength = 5;
+        [SerializeField] private float roomWidth = 10f;
+        [SerializeField] private float roomLength = 10f;
+        [SerializeField] private GameObject rootLevel;
+        [SerializeField] private GameObject roomFloor;
+        
+        private Transform _rootTransform;
+        private Level _level;
+        
+        private void Awake()
+        {
+            _rootTransform = rootLevel.transform;
+            _level = new Level(levelWidth, levelLength);
+        }
+
+        private void Start()
+        {
+            PlaceFloors();
+        }
+
+        private void PlaceFloors()
+        {
+            for (var x = 0; x < _level.rooms.GetLength(0); x++)
+            {
+                for (var z = 0; z < _level.rooms.GetLength(1); z++)
+                {
+                    var position = new Vector3(x * roomWidth, 0, z * roomLength);
+                    Instantiate(roomFloor, position, Quaternion.identity, _rootTransform);
+                }
+            }
+        }
+    }
+}
