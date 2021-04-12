2e601e1ee3d981c399961086e80ef391ca0e1ebc
Brian Maher <bmaher91@gmail.com>
2021-04-12 05:47:34 -0600

    Place all walls

--- Assets/_Project/Scripts/LevelGeneration/LevelGenerator.cs
+++ Assets/_Project/Scripts/LevelGeneration/LevelGenerator.cs
@@ -4,12 +4,17 @@ namespace HuntTheMonster.LevelGeneration
 {
     public class LevelGenerator : MonoBehaviour
     {
+        [Header("Config")]
         [SerializeField] private int levelWidth = 5;
         [SerializeField] private int levelLength = 5;
         [SerializeField] private float roomWidth = 10f;
         [SerializeField] private float roomLength = 10f;
         [SerializeField] private GameObject rootLevel;
+        
+        [Header("Level Components")]
         [SerializeField] private GameObject roomFloor;
+        [SerializeField] private GameObject solidWall;
+        [SerializeField] private GameObject wallWithDoor;
         
         private Transform _rootTransform;
         private Level _level;
@@ -23,17 +28,82 @@ namespace HuntTheMonster.LevelGeneration
         private void Start()
         {
             PlaceFloors();
+            PlaceWalls();
         }
 
         private void PlaceFloors()
         {
-            for (var x = 0; x < _level.rooms.GetLength(0); x++)
+            for (var x = 0; x < _level.Rooms.GetLength(0); x++)
             {
-                for (var z = 0; z < _level.rooms.GetLength(1); z++)
+                for (var y = 0; y < _level.Rooms.GetLength(1); y++)
                 {
-                    var position = new Vector3(x * roomWidth, 0, z * roomLength);
+                    var position = new Vector3(x * roomWidth, 0, y * roomLength);
                     Instantiate(roomFloor, position, Quaternion.identity, _rootTransform);
                 }
+            }
+        }
+
+        private void PlaceWalls()
+        {
+            for (var x = 0; x < _level.Rooms.GetLength(0); x++)
+            {
+                for (var y = 0; y < _level.Rooms.GetLength(1); y++)
+                {
+                    var roomPosition = new Vector3(x * roomWidth, 0, y * roomLength);
+                    var isTopEdge = y == levelLength - 1;
+                    var isRightEdge = x == levelWidth - 1;
+                    var isBottomEdge = y == 0;
+                    var isLeftEdge = x == 0;
+
+                    PlaceTopWall(isTopEdge, roomPosition);
+                    PlaceRightWall(isRightEdge, roomPosition);
+                    PlaceBottomWall(isBottomEdge, roomPosition);
+                    PlaceLeftWall(isLeftEdge, roomPosition);
+                }
+            }
+        }
+
+        private void PlaceLeftWall(bool isLeftEdge, Vector3 roomPosition)
+        {
+            if (isLeftEdge)
+            {
+                var wallRotation = Quaternion.Euler(0, 270, 0);
+                Instantiate(solidWall, roomPosition, wallRotation, _rootTransform);
+            }
+        }
+
+        private void PlaceBottomWall(bool isBottomEdge, Vector3 roomPosition)
+        {
+            if (isBottomEdge)
+            {
+                var wallRotation = Quaternion.Euler(0, 180, 0);
+                Instantiate(solidWall, roomPosition, wallRotation, _rootTransform);
+            }
+        }
+
+        private void PlaceRightWall(bool isRightEdge, Vector3 roomPosition)
+        {
+            if (isRightEdge)
+            {
+                var wallRotation = Quaternion.Euler(0, 90, 0);
+                Instantiate(solidWall, roomPosition, wallRotation, _rootTransform);
+            }
+            else
+            {
+                var wallRotation = Quaternion.Euler(0, 90, 0);
+                Instantiate(wallWithDoor, roomPosition, wallRotation, _rootTransform);
+            }
+        }
+
+        private void PlaceTopWall(bool isTopEdge, Vector3 roomPosition)
+        {
+            if (isTopEdge)
+            {
+                Instantiate(solidWall, roomPosition, Quaternion.identity, _rootTransform);
+            }
+            else
+            {
+                Instantiate(wallWithDoor, roomPosition, Quaternion.identity, _rootTransform);
             }
         }
     }
