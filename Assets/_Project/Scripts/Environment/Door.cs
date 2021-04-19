32c4c13b6ccd9ee192c302312ac6ecc0024f2779
Brian Maher <bmaher91@gmail.com>
2021-04-19 06:22:48 -0600

    Animate door -- also helps player never get stuck in the open door.

--- Assets/_Project/Scripts/Environment/Door.cs
+++ Assets/_Project/Scripts/Environment/Door.cs
@@ -1,15 +1,37 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 namespace HuntTheMonster.Environment
 {
     public class Door : MonoBehaviour
     {
+        [SerializeField] private float openAnimationSeconds = 1f;
+
+        private bool _isOpening;
         private bool _isOpen;
-        
+
         public void Open()
         {
-            if (_isOpen) return;
-            transform.Rotate(new Vector3(0, 90, 0));
+            if (_isOpen || _isOpening) return;
+            StartCoroutine(OpenDoorAnimation());
+        }
+
+        private IEnumerator OpenDoorAnimation()
+        {
+            _isOpening = true;
+
+            var elapsedSeconds = 0f;
+
+            while (elapsedSeconds < openAnimationSeconds)
+            {
+                elapsedSeconds += Time.deltaTime;
+                var rate = Time.deltaTime / openAnimationSeconds;
+                var rotation = new Vector3(0, rate * 90, 0);
+                transform.Rotate(rotation);
+                yield return null;
+            }
+            
+            _isOpening = false;
             _isOpen = true;
         }
     }
